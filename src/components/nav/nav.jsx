954dbb59196e7b5bb7ed0f7f8d7e8c97eee0f49d
Danny <223181@ids.upchiapas.edu.mx>
2024-07-03 01:17:25 -0600

    implementacion a vista movil

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,5 +1,8 @@
+// Dentro de tu componente Nav.js
+
 import React, { useState } from "react";
 import style from "./nav.module.css";
+import { HiMenu } from "react-icons/hi";
 import search from "../../assets/google_web_search.png";
 import user from "../../assets/male_user.png";
 import plus from "../../assets/plus.png";
@@ -7,30 +10,63 @@ import ProfileCard from "../user/user";
 
 export default function Nav() {
     const [showProfile, setShowProfile] = useState(false);
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
     const toggleProfile = () => {
         setShowProfile(!showProfile);
     };
 
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     return (
         <div>
-            <div className={style.nav}>
+            {/* Contenedor del menú horizontal */}
+            <div className={`${style.nav} ${isMobileMenuOpen ? style.hideNav : ''}`}>
                 <a>PLANTCARE</a>
-                
+
                 <div className={style.buscador}>
                     <input className={style.search} type="text" placeholder="Buscar" />
-                    <img src={search} alt="Buscar" />
+                    <img src={search} style={{ position: 'relative', cursor: 'pointer', right: '5px', bottom: '4px' }} alt="Buscar" />
                 </div>
-                
+
                 <a>Inicio</a>
-                <a>Graficas</a>
+                <a>Gráficas</a>
                 <a>Datos</a>
                 <a>Añadir <img src={plus} alt="Añadir" /></a>
-                
+
                 <div className={style.user}>
                     <button onClick={toggleProfile}><img src={user} alt="Usuario" /></button>
                 </div>
             </div>
+
+            {/* Condición para mostrar el ícono de menú en dispositivos móviles */}
+            <div className={`${style.menuToggle} ${isMobileMenuOpen ? style.hideToggle : ''}`} onClick={toggleMobileMenu}>
+                {!isMobileMenuOpen ? <HiMenu /> : <span className={style.closeIcon} onClick={toggleMobileMenu}>&times;</span>}
+            </div>
+
+            {/* Contenedor del menú responsive */}
+            <div className={`${style.menu} ${isMobileMenuOpen ? style.menuOpen : ''}`}>
+                <span className={style.closeIcon} onClick={toggleMobileMenu}>&times;</span>
+                <a>PLANTCARE</a>
+                <a onClick={toggleMobileMenu}>Inicio</a>
+                <a onClick={toggleMobileMenu}>Gráficas</a>
+                <a onClick={toggleMobileMenu}>Datos</a>
+                <a onClick={toggleMobileMenu}>Añadir <img src={plus} alt="Añadir" /></a>
+
+                {/* Contenedor del perfil en dispositivos móviles */}
+                {showProfile && (
+                    <div className={style.profileContainerMovil}>
+                        <ProfileCard name="Nombre Completo" email="Correo electrónico" />
+                    </div>
+                )}
+                <div className={style.user}>
+                    <button onClick={toggleProfile}><img src={user} alt="Usuario" /></button>
+                </div>
+            </div>
+
+            {/* Contenedor del perfil */}
             {showProfile && (
                 <div className={style.profileContainer}>
                     <ProfileCard name="Nombre Completo" email="Correo electrónico" />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Nav from '../../components/nav/nav';
 import Footer from '../../components/footer/footer';
@@ -6,6 +5,7 @@ import CardList from '../../components/homeComponent/cardList/cardList';
 import style from "./Home.module.css";
 import espadaDeRey from '../../assets/espadaderey_4.png';
 import pino from '../../assets/pino_4.png';
+
 
 const plants = [
     { name: "Espada de rey", image: espadaDeRey },
@@ -21,9 +21,7 @@ export default function Home() {
                 <div className={style.header}>
                     <h1 className={style.Jardin}>Mi Jardin</h1>
                     <div className={style.selectContainer}>
-                        
                         <select>
-
                             <option value="nombre">Nombre</option>
                             <option value="tipo">Tipo</option>
                             <option value="fecha">Fecha</option>
@@ -34,6 +32,5 @@ export default function Home() {
             </div>
             <Footer/>
         </div>
-        
     );
 }
