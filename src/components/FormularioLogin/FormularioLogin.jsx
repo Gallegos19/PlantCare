5e66c44dd06196e28df8a17363d3afe6ab131f90
Gallegos19 <223204@ids.upchiapas.edu.mx>
2024-07-02 20:26:35 -0600

    validaciones_formulario_login

--- src/components/FormularioLogin/FormularioLogin.jsx
+++ src/components/FormularioLogin/FormularioLogin.jsx
@@ -2,10 +2,15 @@ import React, { useState, forwardRef, useImperativeHandle } from 'react';
 import Input from '../Input/Input';
 import Button from '../Button/Button';
 import Style from './FormularioLogin.module.css';
+import { useNavigate } from 'react-router-dom';
+import { Snackbar, Alert } from '@mui/material';
 
 const FormularioLogin = forwardRef((props, ref) => {
   const [username, setUsername] = useState('');
   const [contrasena, setContrasena] = useState('');
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
+  const navigate = useNavigate();
 
   // Permite que el padre acceda al ref del formulario
   useImperativeHandle(ref, () => ({
@@ -16,6 +21,23 @@ const FormularioLogin = forwardRef((props, ref) => {
       }
     }
   }));
+
+  const handleEntrar = () => {
+    // Validar que ambos campos estén llenos
+    if (username.trim() !== '' && contrasena.trim() !== '') {
+      navigate('/');
+    } else {
+      setAlertMessage('Por favor, completa todos los campos.');
+      setAlertOpen(true);
+    }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setAlertOpen(false);
+  };
 
   return (
     <div id="formulario-login" className={Style.containerForm}>
@@ -32,10 +54,22 @@ const FormularioLogin = forwardRef((props, ref) => {
         onChange={(newValue) => setContrasena(newValue)}
       />
 
-      <Button title='Entrar' />
+        <div onClick={handleEntrar} >
+        <Button title='Entrar'/>
+        </div>
+     
 
-      <p className={Style.text}> ¿Aun no estas registrado?
-        Registrate aqui</p>
+      <div onClick={() => navigate('/register')}>
+        <p className={Style.text}>
+          ¿Aún no estás registrado? Regístrate aquí
+        </p>
+      </div>
+
+      <Snackbar open={alertOpen} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+          {alertMessage}
+        </Alert>
+      </Snackbar>
     </div>
   );
 });
